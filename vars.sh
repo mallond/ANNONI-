ba7ad16219259c8aef1a557935deadffa8458689
David Mallon ⚙ <mallond@gmail.com>
2023-02-10 14:53:05 +0000

    .

---
+++ vars.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define an empty array
+declare -a my_array
+
+# Load secrets into the array
+secrets=($(echo "${{ secrets.MY_ARRAY }}" | tr ',' '\n'))
+my_array+=("${secrets[@]}")
+
+# Load environment variables into the array
+vars=($(echo "${{ env.MY_ARRAY }}" | tr ',' '\n'))
+my_array+=("${vars[@]}")
+
+# Display the contents of the array
+echo "Array contents:"
+for item in "${my_array[@]}"; do
+  echo " - $item"
+done
